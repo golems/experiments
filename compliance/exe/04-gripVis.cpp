ad6e3db9d34eadefea6b1bb7b6623fd2fa026da6
Can Erdogan <cerdogan3@gatech.edu>
2013-06-18 13:37:26 -0400

    Writing an executable to show the arm motion in grip while moving it in real life as well
    Former-commit-id: 4945a52fdf72c9983506625d49a2558758a4becd

---
+++ compliance/exe/04-gripVis.cpp
@@ -0,0 +1,128 @@
+/**
+ * @file 04-gripVis.cpp
+ * @author Can Erdogan
+ * @date June 18, 2013
+ * @brief This file demonstrates how to visualize the motion of the arms in grip.
+ * NOTE Although I wanted to change this, we had to make the GRIP/wxWidget the main program
+ * (the surrounding thread) and send data within a timer... This could be bad if for some reason
+ * visualization halts and we want to stop the arms right then.
+ */
+
+
+#define protected public
+#define private public
+
+#include "simTab.h"
+#include "GRIPApp.h"
+
+#include "helpers.h"
+#include "initModules.h"
+#include "motion.h"
+
+using namespace std;
+using namespace Eigen;
+using namespace dynamics;
+
+#define parm (cout << llwa.pos[0] << ", " << llwa.pos[1] << ", " << llwa.pos[2] << ", " << \
+	llwa.pos[3] << ", " << llwa.pos[4] << ", " << llwa.pos[5] << ", " << llwa.pos[6] << endl);
+
+/* ********************************************************************************************* */
+somatic_d_t daemon_cx;
+ach_channel_t js_chan;				
+ach_channel_t ft_chan;
+somatic_motor_t llwa;
+
+/* ********************************************************************************************* */
+/// Picks a random configuration for the robot, moves it, does f.k. for the right and left 
+/// end-effectors, places blue and green boxes for their locations and visualizes it all
+void Timer::Notify() {
+
+	// Move the arm to any position with the joystick
+	setJoystickInput(daemon_cx, js_chan, llwa, llwa);
+	somatic_motor_update(&daemon_cx, &llwa);
+
+	// Restart the timer for the next start
+	Start(0.005 * 1e4);	
+}
+
+/* ********************************************************************************************* */
+SimTab::SimTab(wxWindow *parent, const wxWindowID id, const wxPoint& pos, const wxSize& size, 
+		long style) : GRIPTab(parent, id, pos, size, style) {
+
+	// ============================================================================
+	// Initialize grip stuff
+
+  sizerFull = new wxBoxSizer(wxHORIZONTAL);
+	wxBoxSizer* sizerFull = new wxBoxSizer (wxHORIZONTAL);
+	viewer->camRadius = 1.0;
+	viewer->UpdateCamera();
+	SetSizer(sizerFull);
+	frame->DoLoad("../scenes/04-World-Collision.urdf");
+
+	// Create the timer to notify the function that draws the robot at multiple configurations
+	timer = new Timer();
+	timer->world = mWorld;
+	timer->Start(1);	
+
+	// ============================================================================
+	// Initialize robot stuff
+
+	// Initialize this daemon (program!)
+	somatic_d_opts_t dopt;
+	memset(&dopt, 0, sizeof(dopt)); // zero initialize
+	dopt.ident = "01-gripperWeight";
+	somatic_d_init( &daemon_cx, &dopt );
+
+	// Initialize the left arm
+	initArm(daemon_cx, llwa, "llwa");
+
+	// Initialize the joystick channel
+	int r = ach_open(&js_chan, "joystick-data", NULL);
+	aa_hard_assert(r == ACH_OK, "Ach failure '%s' on opening Joystick channel (%s, line %d)\n", 
+		ach_result_to_string(static_cast<ach_status_t>(r)), __FILE__, __LINE__);
+
+	// Send a message; set the event code and the priority
+	somatic_d_event(&daemon_cx, SOMATIC__EVENT__PRIORITIES__NOTICE, 
+			SOMATIC__EVENT__CODES__PROC_RUNNING, NULL, NULL);
+}
+
+/* ********************************************************************************************* */
+SimTab::~SimTab() {
+
+	// Send the stoppig event
+	somatic_d_event(&daemon_cx, SOMATIC__EVENT__PRIORITIES__NOTICE,
+					 SOMATIC__EVENT__CODES__PROC_STOPPING, NULL, NULL);
+
+	// Halt the motors
+	somatic_motor_cmd(&daemon_cx, &llwa, SOMATIC__MOTOR_PARAM__MOTOR_HALT, NULL, 7, NULL);
+	somatic_d_destroy(&daemon_cx);
+}
+
+/* ********************************************************************************************* */
+void SimTab::GRIPEventSimulationBeforeTimestep() {}
+
+/* ********************************************************************************************* */
+// Handler for events
+
+BEGIN_EVENT_TABLE(SimTab, wxPanel)
+	EVT_COMMAND (wxID_ANY, wxEVT_COMMAND_BUTTON_CLICKED, SimTab::OnButton)
+	EVT_COMMAND (wxID_ANY, wxEVT_GRIP_SLIDER_CHANGE, SimTab::OnSlider)
+END_EVENT_TABLE()
+
+/* ********************************************************************************************* */
+// Class constructor for the tab: Each tab will be a subclass of GRIPTab
+
+IMPLEMENT_DYNAMIC_CLASS(SimTab, GRIPTab)
+
+/* ********************************************************************************************* */
+// Necessary interface call to create a GRIP executable 
+
+/// Application class
+extern wxNotebook* tabView;
+class mainApp : public GRIPApp {
+	virtual void AddTabs() {
+		tabView->AddPage(new SimTab(tabView), wxT("Inverse Kinematics"));
+	}
+};
+
+IMPLEMENT_APP(mainApp)
