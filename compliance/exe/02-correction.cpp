b650e0ba0f4cbd7934c8c89e820e6df808dde1f3
Can Erdogan <cerdogan3@gatech.edu>
2013-06-20 11:18:59 -0400

    Decided to model the < 5N error in sensor readings by creating a table before 'optimizing' compliance
    Former-commit-id: 7a3b3cc8c4ce1247936340626714df79ab73093c

--- compliance/exe/02-correction.cpp
+++ compliance/exe/02-correction.cpp
@@ -41,7 +41,7 @@ void run() {
 		c++;
 
 		// Move the arm to any position with the joystick
-		bool setPos = 0;
+		bool setPos = 1;
 		// double q [] = {0.0, -M_PI_2, 0.0, 0.0, M_PI_2, -M_PI_2, 2*M_PI};	
 		double q [] = {0.0, -M_PI_2, 0.0, 0.0, 0.0, 0.0, 2*M_PI};	
 		if(setPos) 
