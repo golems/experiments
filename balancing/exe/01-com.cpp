4eeadb68a33caafb9f1d363259d3e32b714ce9ba
Can Erdogan <cerdogan3@gatech.edu>
2013-07-09 22:14:59 -0400

    Got the com computation and visualization
    Former-commit-id: 4b27f245f9005013ac3c44907191b63aa19e171d

--- balancing/exe/01-com.cpp
+++ balancing/exe/01-com.cpp
@@ -1,15 +1,38 @@
 /**
  * @file 01-com.cpp
- * @author Can Erdogan, Peng Hou
+ * @author Can Erdogan, Peng Hou, Munzir Zafar
  * @date July 08, 2013
- * @brief This executable shows how to estimate the center of mass of the Krang.
+ * @brief This executable shows how to estimate the center of mass of the Krang and visualizes
+ * the total center of mass and individual body nodes center of masses in grip with the data
+ * read from the joints and imu sensor. 
+ * Note if you want to move the joints, please use the home utility.
  */
 
-#include <somatic.h>
-#include <somatic/daemon.h>
+#define protected public
+#define private public
+
+#define RAD2DEG RAD2DEG	// to deal with a define bug in grip which clashes with pcl
+#define DEG2RAD DEG2RAD
+
+#include "somatic.h"
+#include "somatic/daemon.h"
 #include <somatic.pb-c.h>
 #include <somatic/motor.h>
+#include <filter.h>
+
+#include <Eigen/Dense>
+#include <iostream>
+
+#include "GRIPApp.h"
+#include "simTab.h"
 #include "helpers.h"
+
+using namespace std;
+using namespace Eigen;
+using namespace dynamics;
+
+#define NUM_ROWS 480
+#define NUM_COLUMNS 640
 
 #define getMotorMessage(x) (SOMATIC_WAIT_LAST_UNPACK( r, somatic__motor_state, \
 	&protobuf_c_system_allocator, 1024, &x, &abstime))
@@ -17,16 +40,21 @@
 /* ******************************************************************************************** */
 // Communication and filtering
 
-filter_kalman_t *kf;						///< The kalman filter to smooth the imu readings
-somatic_d_t daemon_cx;
-ach_channel_t imuChan;
+filter_kalman_t *kf;					///< the kalman filter to smooth the imu readings
+somatic_d_t daemon_cx;				///< the daemon properties
+ach_channel_t imuChan;				///< the state channel to listen to imu data
 ach_channel_t waistChan;			///< the state channel for the waist module
 ach_channel_t leftArmChan;		///< the state channel for the left arm modules
 ach_channel_t rightArmChan;		///< the state channel for the right arm modules
 
+SkeletonDynamics* robot;			///< the robot representation in dart
+struct timespec t_now, t_prev;///< for timing each iteration
+
+wxStaticText* text;						///< the text box to show the com value
+
 /* ********************************************************************************************* */
 /// Gets the data from the channels
-void getData (Eigen::VectorXd& q, Eigen::VectorXd& dq, double dt, filter_kalman_t* kf) {
+void getData (filter_kalman_t* kf, double dt) {
 
 	// Get the time to get the sensor values by
 	struct timespec currTime;
@@ -40,59 +68,85 @@ void getData (Eigen::VectorXd& q, Eigen::VectorXd& dq, double dt, filter_kalman_
 	assert(((leftArm = getMotorMessage(leftArmChan)) != NULL) && "leftArm call failed");
 	assert(((rightArm = getMotorMessage(rightArmChan)) != NULL) && "rightArm call failed");
 	
-	// Get the data from imu
+	// Get the data from imu 
 	double imu, imuSpeed;
 	getImu(&imuChan, imu, imuSpeed, dt, kf);
 
-	// Store data in the output structure
-	// FIXME: This imu value is not compatible to dart unless changed to -imu+M_PI/2
-	// the current value is compatible to the dynamics.cpp. We need to make dynamics.cpp
-	// compatible to dart
-	q(5) = imu; dq(5) = imuSpeed;
-	q(8) = (waist->position->data[0] - waist->position->data[1])/2.0;
-	dq(8) = (waist->velocity->data[0] - waist->velocity->data[1])/2.0;
-	for (int i = 0; i < 7; i++) {
-		q(10 + 2*i) = leftArm->position->data[i]; dq(10 + 2*i) = leftArm->velocity->data[i];
-		q(11 + 2*i) = rightArm->position->data[i]; dq(11 + 2*i) = rightArm->velocity->data[i];
-	}
+	// Set the imu and waist angles, averaging the waist angle values
+	double waist_val = (waist->position->data[0] - waist->position->data[1]) / 2.0;
+	Vector2d imuWaist_vals (-imu + M_PI_2, waist_val);
+	robot->setConfig(imuWaist_ids, imuWaist_vals);
 	
-	// Filter the data
-	filterState(dt, kf, q, dq);
+	// Update the robot state
+	Vector7d larm_vals = eig7(leftArm->position->data), rarm_vals = eig7(rightArm->position->data);
+	robot->setConfig(left_arm_ids, larm_vals);
+	robot->setConfig(right_arm_ids, rarm_vals);
+
+	// Clean up the messages
+	somatic__motor_state__free_unpacked(leftArm, &protobuf_c_system_allocator);
+	somatic__motor_state__free_unpacked(rightArm, &protobuf_c_system_allocator);
+	somatic__motor_state__free_unpacked(waist, &protobuf_c_system_allocator);
 }
-
-/* ******************************************************************************************** */
-void run() {
-
-	// Send the event massage
-	somatic_d_event(&daemon_cx, SOMATIC__EVENT__PRIORITIES__NOTICE,
-					 SOMATIC__EVENT__CODES__PROC_RUNNING, NULL, NULL);
-
-	// Continue processing the joint values and computing the com until sigterm received
-	struct timespec t_now, t_prev;
-	t_prev = aa_tm_now();
-	Eigen::VectorXd q (24), dq (24), leftFt(6), rightFt(6);
-	while (!somatic_sig_received ) {
-
-		// Get the current time and compute the difference
-		t_now = aa_tm_now();
-		double dt = (double) aa_tm_timespec2sec(aa_tm_sub(t_now, t_prev));
-
-		// Get the data from ach channels and filter
-		getData(q, dq, dt, kf);
-
-		// Compute the center of mass with the sensor data
-
-	}
-
-	// Once the main loop returns, the program is done. Send the stopping event message.
-	somatic_d_event(&daemon_cx, SOMATIC__EVENT__PRIORITIES__NOTICE,
-					 SOMATIC__EVENT__CODES__PROC_STOPPING, NULL, NULL);
-}
-
 
 /* ********************************************************************************************* */
-/// Initializes the ach channels needed to retrieve the state of the robot
-void init () {
+/// Picks a random configuration for the robot, moves it, does f.k. for the right and left 
+/// end-effectors, places blue and green boxes for their locations and visualizes it all
+void Timer::Notify() {
+
+	// Get the current time and compute the difference
+	t_now = aa_tm_now();
+	double dt = (double) aa_tm_timespec2sec(aa_tm_sub(t_now, t_prev));
+	t_prev = t_now;
+
+	// Get the data from ach channels
+	getData(kf, dt);
+
+	// Reflect the changes in the dart kinematics and update the viewer
+	viewer->DrawGLScene();
+
+	// Get the center of mass and balancing angle (theta), and write it to the gui
+	char buf [256];
+	Vector3d com = robot->getWorldCOM();
+	double theta = atan2(com(0), com(2));
+	sprintf(buf, "com (cm): (%lf, %lf, %lf)\n\nbalancing angle (deg): %lf", 100.0 * com(0), 
+		100.0 * com(1), 100.0 * com(2), (theta / M_PI) * 180.0);
+	text->SetLabel(wxString(buf, wxConvUTF8));
+		
+	
+	// Clean up the daemon memory
+	aa_mem_region_release(&daemon_cx.memreg);
+	
+	// Restart the timer for the next start
+	Start(0.005 * 1e4);	
+}
+
+/* ********************************************************************************************* */
+SimTab::SimTab(wxWindow *parent, const wxWindowID id, const wxPoint& pos, const wxSize& size, 
+		long style) : GRIPTab(parent, id, pos, size, style) {
+
+	// ============================================================================
+	// Initialize grip stuff
+
+  sizerFull = new wxBoxSizer(wxHORIZONTAL);
+	wxBoxSizer* sizerFull = new wxBoxSizer (wxHORIZONTAL);
+	viewer->camRadius = 3.0;
+	viewer->worldV += Vector3d(0.0, 0.0, -0.7);
+	viewer->UpdateCamera();
+	SetSizer(sizerFull);
+	frame->DoLoad("../../common/scenes/01-World-Robot.urdf");
+	robot = mWorld->getSkeleton(0);
+
+	// Create a text box to show the center of mass
+ 	text = new wxStaticText(this, wxID_ANY, wxT("(0.0, 0.0, 0.0)"), wxPoint(15, 15));
+  wxFont font(16, wxDEFAULT, wxNORMAL, wxBOLD);
+  text->SetFont(font);
+
+	// Create the timer to notify the function that draws the robot at multiple configurations
+	timer = new Timer();
+	timer->Start(1);	
+
+	// ============================================================================
+	// Initialize robot stuff
 
 	// Initialize this daemon (program!)
 	somatic_d_opts_t dopt;
@@ -111,11 +165,23 @@ void init () {
 	filter_kalman_init(kf, 2, 0, 2);
 	kf->C[0] = kf->C[3] = 1.0;
 	kf->Q[0] = kf->Q[3] = 1e-3;
+
+	// Start the timing
+	t_prev = aa_tm_now();
+
+	// Send a message; set the event code and the priority
+	somatic_d_event(&daemon_cx, SOMATIC__EVENT__PRIORITIES__NOTICE, 
+			SOMATIC__EVENT__CODES__PROC_RUNNING, NULL, NULL);
 }
 
 /* ********************************************************************************************* */
-/// Destroy the channel and daemon resources
-void destroy () {
+SimTab::~SimTab() {
+	
+	// Send the stopping event
+	somatic_d_event(&daemon_cx, SOMATIC__EVENT__PRIORITIES__NOTICE,
+					 SOMATIC__EVENT__CODES__PROC_STOPPING, NULL, NULL);
+  
+	// Destroy the channel and daemon resources
 	somatic_d_channel_close(&daemon_cx, &imuChan);
 	somatic_d_channel_close(&daemon_cx, &waistChan);	 
 	somatic_d_channel_close(&daemon_cx, &leftArmChan); 
@@ -124,10 +190,30 @@ void destroy () {
 }
 
 /* ********************************************************************************************* */
-/// The main thread
-int main() {
-	init();
-	run();
-	destroy();
-	return 0;
-}
+void SimTab::GRIPEventSimulationBeforeTimestep() {}
+
+/* ********************************************************************************************* */
+// Handler for events
+
+BEGIN_EVENT_TABLE(SimTab, wxPanel)
+	EVT_COMMAND (wxID_ANY, wxEVT_COMMAND_BUTTON_CLICKED, SimTab::OnButton)
+	EVT_COMMAND (wxID_ANY, wxEVT_GRIP_SLIDER_CHANGE, SimTab::OnSlider)
+END_EVENT_TABLE()
+
+/* ********************************************************************************************* */
+// Class constructor for the tab: Each tab will be a subclass of GRIPTab
+
+IMPLEMENT_DYNAMIC_CLASS(SimTab, GRIPTab)
+
+/* ********************************************************************************************* */
+// Necessary interface call to create a GRIP executable 
+
+/// Application class
+extern wxNotebook* tabView;
+class mainApp : public GRIPApp {
+	virtual void AddTabs() {
+		tabView->AddPage(new SimTab(tabView), wxT("Center of mass"));
+	}
+};
+
+IMPLEMENT_APP(mainApp)
