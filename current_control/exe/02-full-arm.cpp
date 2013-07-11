e4cc179604e25451fefc3c48093f746579e76506
Saul Reynolds-Haertle <sreynoldshaertle@gmail.com>
2013-07-11 03:52:48 +0100

    fix some math, make pid state maintenance cleaner
    Former-commit-id: 8d32a420483591091998fdb37e31f42f47fe0b36

--- current_control/exe/02-full-arm.cpp
+++ current_control/exe/02-full-arm.cpp
@@ -16,14 +16,32 @@
 #include <fstream>
 #include <ncurses.h>
 #include <iomanip>
+#include <cmath>
+
+// ################################################################################
+// constants
+
+#define PID_ERROR_WINDOW_SIZE 30
+
+bool do_curses = true;
+
+bool use_pos[] = {true, true, true, true, true, true, true};
+bool use_vel[] = {true, true, true, true, true, true, true};
+
+double init_K_p_p[] = {15.0,  15.0, 15.0, 12.0, 15.0,  7.0,  7.0};
+double init_K_p_d[] = {0.0,   0.0,  0.0,  0.0,  0.0,  0.0,  0.0};
+double init_K_v_p[] = {1.0,   1.0,  1.0,  1.0,  1.0,  1.0,  1.0};
+double init_K_v_d[] = {0.0,   0.0,  0.0,  0.0,  0.0,  0.0,  0.0};
+
+// double init_pos_limits[] = {2*M_PI/3, 2*M_PI/3, 2*M_PI/3, 2*M_PI/3, 2*M_PI/3, 2*M_PI/3, 2*M_PI/3};
+// double init_vel_limits[] = {1.0,      1.0,      1.0,      1.0,      1.0,      1.0,      1.0};
+
+double joint_vel_command_scale = 1.0;
+double joint_cur_command_scale = 8.0;
 
 // ################################################################################
 // definitions
 
-typedef enum {
-    JOYSTICK_MODE_POSITION = 0,
-    JOYSTICK_MODE_DIRECT
-} joystick_mode_t;
 typedef enum {
     JOYSTICK_CONTROLLING_NONE = 0,
     JOYSTICK_CONTROLLING_LEFT_SMALL,
@@ -31,6 +49,26 @@ typedef enum {
     JOYSTICK_CONTROLLING_RIGHT_SMALL,
     JOYSTICK_CONTROLLING_RIGHT_LARGE
 } joystick_controlling_t;
+
+typedef struct {
+    bool use_pos;
+    bool use_vel;
+
+    double pos_target;
+    double vel_target;
+
+    // double pos_limit;
+    // double vel_limit;
+
+    double K_p_p;
+    double K_p_d;
+    double K_v_p;
+    double K_v_d;
+    double pos_error_last;
+    double vel_error_last;
+    double pos_error_window[PID_ERROR_WINDOW_SIZE];
+    double vel_error_window[PID_ERROR_WINDOW_SIZE];
+} pid_state_t;
 
 // ################################################################################
 // global variables
@@ -43,18 +81,10 @@ somatic_motor_t rlwa;
 somatic_motor_t llwa;
 
 double last_js_time;
-joystick_mode_t js_mode;
 joystick_controlling_t js_controlling;
 Somatic__Joystick* last_js_msg;
 
-// ################################################################################
-// constants
-
-float K_p[] = {15.0, 15.0, 15.0, 12.0, 15.0, 7.0, 7.0};
-float K_d[] = {0.0,  0.0,  0.0,  0.0,  0.0,  0.0, 0.0};
-
-double joint_vel_command_scale = 0.30;
-double joint_cur_command_scale = 8.0;
+bool halted = true;
 
 // ################################################################################
 // small helper functions
@@ -72,7 +102,8 @@ double gettime() {
 // ################################################################################
 // display
 
-double do_init_curses() {
+void do_init_curses() {
+    if (!do_curses) return;
     initscr();
     clear();
     noecho();                   // do not echo input to the screen
@@ -80,7 +111,8 @@ double do_init_curses() {
     timeout(0);                 // non-blocking getch
 }
 
-double do_end_curses() {
+void do_end_curses() {
+    if (!do_curses) return;
     clrtoeol();
     refresh();
     endwin();
@@ -88,7 +120,9 @@ double do_end_curses() {
 
 #define AXIS_SLIDER_WIDTH 20
 char input_display_buffer[(AXIS_SLIDER_WIDTH * 2) + 1];
-double do_input_display(Somatic__Joystick* js_msg) {
+void do_input_display(Somatic__Joystick* js_msg) {
+    if (!do_curses) return;
+
     // column of ten buttons in the top middle
     mvprintw(1, 20, "buttons");
     for(int i = 0; i < 10; i++) {
@@ -128,45 +162,50 @@ double do_input_display(Somatic__Joystick* js_msg) {
         mvprintw(6, 2, "RIGHT LARGE");
         break;
     }
-
-    mvprintw(8, 1, "mode");
-    for(int i = 9; i <= 10; i++) mvprintw(i, 2, "            ");
-    switch(js_mode) {
-    case JOYSTICK_MODE_POSITION:
-        mvprintw(9, 2, "POSITION");
-        break;
-    case JOYSTICK_MODE_DIRECT:
-        mvprintw(10, 2, "DIRECT");
-        break;
-    }
 }
 
-double do_control_display(somatic_motor_t* llwa, somatic_motor_t* rlwa, 
-                          double* ltarget_pos, double* ltarget_vel,
-                          double* rtarget_pos, double* rtarget_vel,
-                          double* lmessage,
-                          double* rmessage) {
+void do_control_display(somatic_motor_t* llwa, somatic_motor_t* rlwa,
+                        pid_state_t* lpids, pid_state_t* rpids,
+                        double* lmessage, double* rmessage) {
+    if (!do_curses) return;
+
+    // whether we're halted
+    if (halted) { mvprintw(8, 2, "HALTED"); }
+    else { mvprintw(8, 2, "      "); }
 
     // top row is for left arm, bottom row is right arm
     // from left to right: position target, position actual, velocity target, velocity actual, and commanded current
-    mvprintw(15, 1, "left pos: target, actual");
-    for(int i = 0; i < 7; i++) mvprintw(16+i, 2, "%f", ltarget_pos[i]);
+
+    mvprintw(15, 1, "left pos: target, actual, error");
+    for(int i = 0; i < 7; i++) mvprintw(16+i, 2, "%f", lpids[i].pos_target);
     for(int i = 0; i < 7; i++) mvprintw(16+i, 22, "%f", llwa->pos[i]);
-    mvprintw(24, 1, "right pos: target, actual");
-    for(int i = 0; i < 7; i++) mvprintw(25+i, 2, "%f", rtarget_pos[i]);
+    for(int i = 0; i < 7; i++) mvprintw(16+i, 42, "%f", lpids[i].pos_target - llwa->pos[i]);
+    mvprintw(24, 1, "right pos: target, actual, error");
+    for(int i = 0; i < 7; i++) mvprintw(25+i, 2, "%f", rpids[i].pos_target);
     for(int i = 0; i < 7; i++) mvprintw(25+i, 22, "%f", rlwa->pos[i]);
+    for(int i = 0; i < 7; i++) mvprintw(25+i, 42, "%f", rpids[i].pos_target - rlwa->pos[i]);
 
-    mvprintw(15, 41, "left vel: target, actual");
-    for(int i = 0; i < 7; i++) mvprintw(16+i, 42, "%f", ltarget_vel[i]);
-    for(int i = 0; i < 7; i++) mvprintw(16+i, 62, "%f", llwa->vel[i]);
-    mvprintw(24, 41, "right vel: target, actual");
-    for(int i = 0; i < 7; i++) mvprintw(25+i, 42, "%f", rtarget_vel[i]);
-    for(int i = 0; i < 7; i++) mvprintw(25+i, 62, "%f", rlwa->vel[i]);
+    mvprintw(15, 61, "left vel: target, actual, error");
+    for(int i = 0; i < 7; i++) mvprintw(16+i, 62, "%f", lpids[i].vel_target);
+    for(int i = 0; i < 7; i++) mvprintw(16+i, 82, "%f", llwa->vel[i]);
+    for(int i = 0; i < 7; i++) mvprintw(16+i, 102, "%f", lpids[i].vel_target - llwa->vel[i]);
+    mvprintw(24, 61, "right vel: target, actual, error");
+    for(int i = 0; i < 7; i++) mvprintw(25+i, 62, "%f", rpids[i].vel_target);
+    for(int i = 0; i < 7; i++) mvprintw(25+i, 82, "%f", rlwa->vel[i]);
+    for(int i = 0; i < 7; i++) mvprintw(25+i, 102, "%f", rpids[i].vel_target - rlwa->vel[i]);
 
-    mvprintw(15, 81, "left commanded current");
-    for(int i = 0; i < 7; i++) mvprintw(16+i, 82, "%f", lmessage[i]);
-    mvprintw(24, 81, "right commanded current");
-    for(int i = 0; i < 7; i++) mvprintw(25+i, 82, "%f", rmessage[i]);
+    mvprintw(15, 121, "left commanded current");
+    for(int i = 0; i < 7; i++) mvprintw(16+i, 122, "%f", lmessage[i]);
+    mvprintw(24, 121, "right commanded current");
+    for(int i = 0; i < 7; i++) mvprintw(25+i, 122, "%f", rmessage[i]);
+
+    // and on th eobttom, display gains
+    mvprintw(33, 1, "pos gains: p, d");
+    for(int i = 0; i < 7; i++) mvprintw(34+i, 2, "%f", lpids[i].K_p_p);
+    for(int i = 0; i < 7; i++) mvprintw(34+i, 22, "%f", lpids[i].K_p_d);
+    mvprintw(33, 41, "vel gains: p, d");
+    for(int i = 0; i < 7; i++) mvprintw(34+i, 42, "%f", lpids[i].K_v_p);
+    for(int i = 0; i < 7; i++) mvprintw(34+i, 62, "%f", lpids[i].K_v_d);
 }
 
 // ################################################################################
@@ -195,8 +234,7 @@ void do_init_joystick() {
     }
 }
 
-int do_joystick(double* ltarget_pos, double* ltarget_vel, double* rtarget_pos, double* rtarget_vel,
-                double* lmessage, double* rmessage) {
+int do_joystick(pid_state_t* lpids, pid_state_t* rpids) {
     int r;
     Somatic__Joystick* js_msg = SOMATIC_GET_LAST_UNPACK(r, somatic__joystick, &protobuf_c_system_allocator, 4096, &js_chan);
     if(!(ACH_OK == r || ACH_MISSED_FRAME == r) || (js_msg == NULL)) {
@@ -208,7 +246,7 @@ int do_joystick(double* ltarget_pos, double* ltarget_vel, double* rtarget_pos, d
     double js_dT = cur_js_time - last_js_time;
     last_js_time = cur_js_time;
 
-    // figure out what mode we're in
+    // figure out what we're controlling
     js_controlling = JOYSTICK_CONTROLLING_NONE;
     if (js_msg->buttons->data[4] && !js_msg->buttons->data[5] && !js_msg->buttons->data[6] && !js_msg->buttons->data[7]) {
         js_controlling = JOYSTICK_CONTROLLING_LEFT_SMALL;
@@ -222,43 +260,62 @@ int do_joystick(double* ltarget_pos, double* ltarget_vel, double* rtarget_pos, d
     if (!js_msg->buttons->data[4] && !js_msg->buttons->data[5] && !js_msg->buttons->data[6] && js_msg->buttons->data[7]) {
         js_controlling = JOYSTICK_CONTROLLING_RIGHT_LARGE;
     }
+
+    if (js_msg->buttons->data[1] && !last_js_msg->buttons->data[1]) {
+        for (int i = 0; i < 7; i++) { lpids[i].K_v_p -= .1; rpids[i].K_v_p -= .1; }
+    }
     if (js_msg->buttons->data[3] && !last_js_msg->buttons->data[3]) {
-        if (js_mode == JOYSTICK_MODE_POSITION) { js_mode = JOYSTICK_MODE_DIRECT; }
+        for (int i = 0; i < 7; i++) { lpids[i].K_v_p += .1; rpids[i].K_v_p += .1; }
+    }
+    if (js_msg->buttons->data[0] && !last_js_msg->buttons->data[0]) {
+        for (int i = 0; i < 7; i++) { lpids[i].K_v_d -= .01; rpids[i].K_v_d -= .01; }
+    }
+    if (js_msg->buttons->data[2] && !last_js_msg->buttons->data[2]) {
+        for (int i = 0; i < 7; i++) { lpids[i].K_v_d += .01; rpids[i].K_v_d += .01; }
+    }
+
+    // if (js_msg->buttons->data[1] && !last_js_msg->buttons->data[1]) { for(int i = 0; i < 7; i++) K_v_p[i] -= .1; } // minus gains
+    // if (js_msg->buttons->data[3] && !last_js_msg->buttons->data[3]) { for(int i = 0; i < 7; i++) K_v_p[i] += .1; } // plus gains
+    // if (js_msg->buttons->data[0] && !last_js_msg->buttons->data[0]) { for(int i = 0; i < 7; i++) K_v_d[i] -= .01; } // minus gains
+    // if (js_msg->buttons->data[2] && !last_js_msg->buttons->data[2]) { for(int i = 0; i < 7; i++) K_v_d[i] += .01; } // plus gains
+
+    if (js_msg->buttons->data[8] && !last_js_msg->buttons->data[8]) { // brakes!
+        halted = !halted;
+        if (halted) {
+            somatic_motor_cmd(&daemon_cx, &llwa, SOMATIC__MOTOR_PARAM__MOTOR_HALT, NULL, 7, NULL);
+            somatic_motor_cmd(&daemon_cx, &rlwa, SOMATIC__MOTOR_PARAM__MOTOR_HALT, NULL, 7, NULL);
+        }
         else {
-            for (int i = 0; i < 7; i++) ltarget_pos[i] = llwa.pos[i];
-            for (int i = 0; i < 7; i++) rtarget_pos[i] = rlwa.pos[i];
-            js_mode = JOYSTICK_MODE_POSITION;
+            somatic_motor_cmd(&daemon_cx, &llwa, SOMATIC__MOTOR_PARAM__MOTOR_RESET, NULL, 7, NULL);
+            somatic_motor_cmd(&daemon_cx, &rlwa, SOMATIC__MOTOR_PARAM__MOTOR_RESET, NULL, 7, NULL);
         }
     }
+    if (js_msg->buttons->data[9] && !last_js_msg->buttons->data[9]) { somatic_sig_received = true; } // quit
 
     // do what we want with the joystick message
     switch(js_controlling) {
     case JOYSTICK_CONTROLLING_LEFT_SMALL:
         for(int i = 0; i < 3; i++) {
-            lmessage[i+4] = js_msg->axes->data[i] * joint_cur_command_scale; // will be overwritten by pid in position mode
-            ltarget_vel[i+4] = js_msg->axes->data[i] * joint_vel_command_scale;
-            ltarget_pos[i+4] += js_dT * js_msg->axes->data[i] * joint_vel_command_scale;
+            lpids[i+4].vel_target = js_msg->axes->data[i] * joint_vel_command_scale;
+            lpids[i+4].pos_target += js_dT * js_msg->axes->data[i] * joint_vel_command_scale;
         }
         break;
     case JOYSTICK_CONTROLLING_LEFT_LARGE:
         for(int i = 0; i < 4; i++) {
-            lmessage[i] = js_msg->axes->data[i] * joint_cur_command_scale; // will be overwritten by pid in position mode
-            ltarget_vel[i] = js_msg->axes->data[i] * joint_vel_command_scale;
-            ltarget_pos[i] += js_dT * js_msg->axes->data[i] * joint_vel_command_scale;
+            lpids[i].vel_target = js_msg->axes->data[i] * joint_vel_command_scale;
+            lpids[i].pos_target += js_dT * js_msg->axes->data[i] * joint_vel_command_scale;
         }
         break;
     case JOYSTICK_CONTROLLING_RIGHT_SMALL:
         for(int i = 0; i < 3; i++) {
-            rmessage[i+4] = js_msg->axes->data[i] * joint_cur_command_scale; // will be overwritten by pid in position mode
-            rtarget_vel[i+4] = js_msg->axes->data[i] * joint_vel_command_scale;
-            rtarget_pos[i+4] += js_dT * js_msg->axes->data[i] * joint_vel_command_scale;
+            rpids[i+4].vel_target = js_msg->axes->data[i] * joint_vel_command_scale;
+            rpids[i+4].pos_target += js_dT * js_msg->axes->data[i] * joint_vel_command_scale;
         }
         break;
     case JOYSTICK_CONTROLLING_RIGHT_LARGE:
         for(int i = 0; i < 4; i++) {
-            rmessage[i] = js_msg->axes->data[i] * joint_cur_command_scale; // will be overwritten by pid in position mode
-            rtarget_vel[i] = js_msg->axes->data[i] * joint_vel_command_scale;
-            rtarget_pos[i] += js_dT * js_msg->axes->data[i] * joint_vel_command_scale;
+            rpids[i].vel_target = js_msg->axes->data[i] * joint_vel_command_scale;
+            rpids[i].pos_target += js_dT * js_msg->axes->data[i] * joint_vel_command_scale;
         }
         break;
     }
@@ -273,14 +330,67 @@ int do_joystick(double* ltarget_pos, double* ltarget_vel, double* rtarget_pos, d
     return 0;
 }
 
-double do_pid(somatic_motor_t* lwa, double* target_pos, double* target_vel, double* message) {
-    double p_value;
-    double d_value;
+void do_init_pids(somatic_motor_t* mot, pid_state_t* pids) {
+    for(int i = 0; i < mot->n; i++) {
+        pids[i].use_pos = use_pos[i];
+        pids[i].use_vel = use_vel[i];
+        pids[i].pos_target = 0.0;
+        pids[i].vel_target = 0.0;
+        pids[i].pos_error_last = 0.0;
+        pids[i].vel_error_last = 0.0;
+        for(int j = 0; j < PID_ERROR_WINDOW_SIZE; j++) pids[i].pos_error_window[j] = 0.0;
+        for(int j = 0; j < PID_ERROR_WINDOW_SIZE; j++) pids[i].vel_error_window[j] = 0.0;
+        pids[i].pos_target = mot->pos[i];
+        
+        pids[i].K_p_p = init_K_p_p[i];
+        pids[i].K_p_d = init_K_p_d[i];
+        pids[i].K_v_p = init_K_v_p[i];
+        pids[i].K_v_d = init_K_v_d[i];
 
-    for (int i = 0; i < 7; i++) {
-        p_value = K_p[i] * (target_pos[i] - lwa->pos[i]);
-        d_value = K_d[i] * (target_vel[i] - lwa->vel[i]);
-        message[i] = p_value + d_value;
+        // pids[i].pos_limit = init_pos_limits[i];
+        // pids[i].vel_limit = init_vel_limits[i];
+    }
+}
+
+void update_pids(somatic_motor_t* mot, pid_state_t* pids, double* result) {
+    double p_p_value;
+    double p_d_value;
+    double v_p_value;
+    double v_d_value;
+
+    double pos_error;
+    double vel_error;
+
+    for(int i = 0; i < mot->n; i++) {
+        result[i] = 0;
+
+        // if (fabs(pids[i].pos_target) > fabs(pids[i].pos_limit)) {
+        //     pids[i].pos_target = copysign(pids[i].pos_limit, pids[i].pos_target);
+        // }
+        // if (fabs(pids[i].vel_target) > fabs(pids[i].vel_limit)) {
+        //     pids[i].vel_target = copysign(pids[i].vel_limit, pids[i].vel_target);
+        // }
+
+        if(pids[i].use_pos) {
+            pos_error = pids[i].pos_target - mot->pos[i];
+
+            p_p_value = pids[i].K_p_p * pos_error;
+            p_d_value = pids[i].K_p_d * (pos_error - pids[i].pos_error_last);
+
+            result[i] += p_p_value + p_d_value;
+
+            pids[i].pos_error_last = pos_error;
+        }
+        if (pids[i].use_vel) {
+            vel_error = pids[i].vel_target - mot->vel[i];
+
+            v_p_value = pids[i].K_v_p * vel_error;
+            v_d_value = pids[i].K_v_d * (vel_error - pids[i].vel_error_last);
+
+            result[i] += v_p_value + v_d_value;
+
+            pids[i].vel_error_last = vel_error;
+        }
     }
 }
 
@@ -291,11 +401,9 @@ int main( int argc, char **argv ) {
     // variables
     double lmessage[7] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
     double rmessage[7] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-    
-    double ltarget_pos[7] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-    double ltarget_vel[7] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-    double rtarget_pos[7] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-    double rtarget_vel[7] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+
+    pid_state_t lpids[7];
+    pid_state_t rpids[7];
 
     // init curses
     do_init_curses();
@@ -311,8 +419,8 @@ int main( int argc, char **argv ) {
     somatic_motor_init(&daemon_cx, &rlwa, 7, "rlwa-cmd", "rlwa-state");
     somatic_motor_update(&daemon_cx, &llwa);
     somatic_motor_update(&daemon_cx, &rlwa);
-    somatic_motor_cmd(&daemon_cx, &llwa, SOMATIC__MOTOR_PARAM__MOTOR_RESET, NULL, 7, NULL);
-    somatic_motor_cmd(&daemon_cx, &rlwa, SOMATIC__MOTOR_PARAM__MOTOR_RESET, NULL, 7, NULL);
+    // somatic_motor_cmd(&daemon_cx, &llwa, SOMATIC__MOTOR_PARAM__MOTOR_RESET, NULL, 7, NULL);
+    // somatic_motor_cmd(&daemon_cx, &rlwa, SOMATIC__MOTOR_PARAM__MOTOR_RESET, NULL, 7, NULL);
     usleep(1e5);
 
     // set limits
@@ -328,15 +436,17 @@ int main( int argc, char **argv ) {
     // start our targets at where the motors actually are
     somatic_motor_update(&daemon_cx, &llwa);
     somatic_motor_update(&daemon_cx, &rlwa);
-    for (int i = 0; i < 7; i++) ltarget_pos[i] = llwa.pos[i];
-    for (int i = 0; i < 7; i++) rtarget_pos[i] = rlwa.pos[i];
+    do_init_pids(&llwa, lpids);
+    do_init_pids(&rlwa, rpids);
 
     // main loop
     while(!somatic_sig_received) {
         // do curses stuff
-        int ch = getch();
-        switch (ch) {
-        case 'q': somatic_sig_received = true; break;
+        if(do_curses) {
+            int ch = getch();
+            switch (ch) {
+            case 'q': somatic_sig_received = true; break;
+            }
         }
 
         // update motor
@@ -344,21 +454,21 @@ int main( int argc, char **argv ) {
         somatic_motor_update(&daemon_cx, &rlwa);
 
         // update joystick
-        do_joystick(ltarget_pos, ltarget_vel, rtarget_pos, rtarget_vel, lmessage, rmessage);
+        do_joystick(lpids, rpids);
         
         // do the pid control thing
-        if (js_mode == JOYSTICK_MODE_POSITION) {
-            do_pid(&llwa, ltarget_pos, ltarget_vel, lmessage);
-            do_pid(&rlwa, rtarget_pos, rtarget_vel, rmessage);
-        }
+        update_pids(&llwa, lpids, lmessage);
+        update_pids(&rlwa, rpids, rmessage);
 
         // display it all
-        do_control_display(&llwa, &rlwa, ltarget_pos, ltarget_vel, rtarget_pos, rtarget_vel, lmessage, rmessage);
+        do_control_display(&llwa, &rlwa, lpids, rpids, lmessage, rmessage);
         refresh();
 
         // and send commands
-        somatic_motor_cmd(&daemon_cx, &llwa, SOMATIC__MOTOR_PARAM__MOTOR_CURRENT, lmessage, 7, NULL);
-        somatic_motor_cmd(&daemon_cx, &rlwa, SOMATIC__MOTOR_PARAM__MOTOR_CURRENT, rmessage, 7, NULL);
+        if (!halted) {
+            // somatic_motor_cmd(&daemon_cx, &llwa, SOMATIC__MOTOR_PARAM__MOTOR_CURRENT, lmessage, 7, NULL);
+            somatic_motor_cmd(&daemon_cx, &rlwa, SOMATIC__MOTOR_PARAM__MOTOR_CURRENT, rmessage, 7, NULL);
+        }
     }
 
     // Send stopping event
