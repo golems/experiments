c33d3d38997a28eb704014cc10c7fd8648bfc963
Can Erdogan <canerdogan.89@gmail.com>
2013-07-17 10:36:01 -0400

    logging in the 06-wheelComply
    Former-commit-id: 3fdbd24c2f27d0ee8fd050cea40326da4b097730

--- balancing/matlab/plotWheelComply.m
+++ balancing/matlab/plotWheelComply.m
@@ -1,13 +1,27 @@
 figure;
-plot(bla(:, end), bla(:, 1), 'r'); hold on;
-plot(bla(:, end), bla(:, 2), 'k'); hold on;
-plot(bla(:, end), bla(:, 3), 'g'); hold on;
-plot(bla(:, end), bla(:, 4), 'y'); hold on;
-plot(bla(:, end), bla(:, 5), 'c'); hold on;
-plot(bla(:, end), bla(:, 6)*180/pi, 'b'); hold on;
-plot(bla(:, end), bla(:, 7)*180/pi, 'm'); hold on;
-plot(bla(:, end), bla(:, 8)*180/pi, 'k');
-legend('mean torque', 'torque', 'current cmd', 'curent left', 'current right', 'theta', 
-	'theta_{ref}', 'theta_{error}');
+colors = jet(15);
+for i = 1:12;
+	plot(bla(:,end), bla(:,i), 'color', colors(i,:)); hold on
+end
+plot(bla(:,end), bla(:,7)-bla(:,10), 'color', colors(13,:));
+plot(bla(:,end), bla(:,8)-bla(:,11), 'color', colors(14,:));
+plot(bla(:,end), bla(:,9)-bla(:,12), 'color', colors(15,:));
 
+str{1} = 'Avg Torque due to external force';
+str{2} = 'Raw Torque due to external force';
+str{3} = 'Left Wheel Current Cmd';
+str{4} = 'Right Wheel Current Cmd';
+str{5} = 'Left Wheel Raw Current';
+str{6} = 'Right Wheel Raw Current';
+str{7} = 'Theta';
+str{8} = 'x';
+str{9} = 'psi';
+str{10} = 'Theta_{ref}';
+str{11} = 'x_{ref}';
+str{12} = 'psi_{ref}';
+str{13} = 'Theta_{error}';
+str{14} = 'x_{error}';
+str{15} = 'psi_{error}';
+
+legend(str);
 title('Force Responsive Balancing')
