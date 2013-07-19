0c193463ae36dfd5405b6ab489f536087258740a
jkscholz <jonathan.scholz@gmail.com>
2013-07-18 21:22:29 -0400

    Add licenses to some files
    Former-commit-id: c5ecb2877c670f2e558acf606f489a371ad51337

--- teleop/src/WorkspaceControl.h
+++ teleop/src/WorkspaceControl.h
@@ -1,8 +1,42 @@
+/* -*- mode: C; c-basic-offset: 4  -*- */
 /*
- * WorkspaceTeleop.h
+ * Copyright (c) 2013, Georgia Tech Research Corporation
+ * All rights reserved.
  *
- *  Created on: Jul 17, 2013
- *      Author: jscholz
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ *     * Neither the name of the Georgia Tech Research Corporation nor
+ *       the names of its contributors may be used to endorse or
+ *       promote products derived from this software without specific
+ *       prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY GEORGIA TECH RESEARCH CORPORATION ''AS
+ * IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL GEORGIA
+ * TECH RESEARCH CORPORATION BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+/** \file WorkspaceControl.h
+ *
+ *  \author Jonathan Scholz
+ *  \date 7/17/2013
  */
 
 #ifndef WORKSPACETELEOP_H_
@@ -13,8 +47,6 @@
 #include <kinematics/BodyNode.h>
 
 #include "KrangControl.h"
-
-
 
 /*
  *
