18abed757e6f12151359612a82e4e8ac5ea9b328
Can Erdogan <canerdogan.89@gmail.com>
2013-07-24 13:31:08 -0400

    Got the direction of the liberty correctly - had to map quaternion to rpy and then some hacking
    Former-commit-id: f46d1c7aa8f17f38158449c8c8fc4718c577da8f

--- teleop/exe/02-liberty-display.cpp
+++ teleop/exe/02-liberty-display.cpp
@@ -1,5 +1,5 @@
 /**
- * @file 04-gripVis.cpp
+ * @file 02-liberty-display.cpp
  * @author Can Erdogan
  * @date June 18, 2013
  * @brief This file demonstrates how to visualize the motion of the arms in grip.
@@ -11,7 +11,6 @@
 
 #define protected public
 #define private public
-
 
 #include <GRIPApp.h>
 #include <somatic.h>
@@ -30,118 +29,106 @@ using namespace std;
 using namespace Eigen;
 using namespace dynamics;
 
+#define pv(a) std::cout << #a << ": " << fix((a).transpose()) << std::endl
+
+/* ******************************************************************************************** */
+Eigen::MatrixXd fix (const Eigen::MatrixXd& mat) {
+	Eigen::MatrixXd mat2 (mat);
+	for(size_t i = 0; i < mat2.rows(); i++)
+		for(size_t j = 0; j < mat2.cols(); j++)
+			if(fabs(mat2(i,j)) < 1e-5) mat2(i,j) = 0.0;
+	return mat2;
+}
+
 /* ********************************************************************************************* */
-
-// somatic daemon
 somatic_d_t daemon_cx;
+ach_channel_t liberty_chan;
 
-// Liberty client
-LibertyClient liberty;
+/* ********************************************************************************************* */
+/// Returns the values of the first liberty sensor
+bool getLiberty(VectorXd& config) {
 
-enum DynamicSimulationTabEvents {
-	id_button_ResetLiberty = 8100,
-	id_checkbox_ToggleLibertyRelMode,
-};
+	// Get the data
+	int r = 0;
+	config.setZero(); 
+	Somatic__Liberty *l_msg = SOMATIC_GET_LAST_UNPACK( r, somatic__liberty,
+			&protobuf_c_system_allocator, 1024, &liberty_chan );
+	if(!(ACH_OK == r || ACH_MISSED_FRAME == r) || (l_msg == NULL)) return false;
 
-static bool liberty_rel_mode = 0;
+	// Set the values for the position
+	Somatic__Vector* s1 = l_msg->sensor1;
+	config << s1->data[0], -s1->data[1], -s1->data[2], 0.0, 0.0, 0.0;
+
+	// Convert from a quaternion to rpy representation
+	Quaternion <double> oriQ (s1->data[6], s1->data[3], s1->data[4], s1->data[5]);
+	Matrix3d oriM = oriQ.matrix();
+	Vector3d oriE = math::matrixToEuler(oriM, math::XYZ);
+	config.bottomLeftCorner<3,1>() << -oriE[2], -oriE[1], oriE[0];
+	return true;
+}
 
 /* ********************************************************************************************* */
 /// Picks a random configuration for the robot, moves it, does f.k. for the right and left 
 /// end-effectors, places blue and green boxes for their locations and visualizes it all
 void Timer::Notify() {
 	
-	std::vector<Matrix4d> poses = liberty_rel_mode ? liberty.getRelPoses() : liberty.getRawPoses();
-	for (int i=0; i < 8; ++i) {
-		cout << poses[i] << endl;
-		VectorXd arrowConf = transformToEuler(poses[i], math::XYZ);
-		mWorld->getSkeleton(i)->setConfig(dartRootDofOrdering, arrowConf);
-	}
+	// Get the liberty info
+	VectorXd liberty_input (6);
+	bool result = false;
+	while(!result) result = getLiberty(liberty_input);
+	pv(liberty_input);
 
-    // Restart the timer for the next start
-    viewer->DrawGLScene();
+//	std::vector<Matrix4d> poses = liberty_rel_mode ? liberty.getRelPoses() : liberty.getRawPoses();
+//	for (int i=0; i < 8; ++i) {
+//		cout << poses[i] << endl;
+//		VectorXd arrowConf = transformToEuler(poses[i], math::XYZ);
+//		mWorld->getSkeleton(i)->setConfig(dartRootDofOrdering, arrowConf);
+//	}
+
+	// Restart the timer for the next start
+	viewer->DrawGLScene();
+	Start(0.001 * 1e4);
 }
 
 /* ********************************************************************************************* */
 SimTab::SimTab(wxWindow *parent, const wxWindowID id, const wxPoint& pos, const wxSize& size, 
-	       long style) : GRIPTab(parent, id, pos, size, style) {
+				 long style) : GRIPTab(parent, id, pos, size, style) {
 
+	// ============================================================================
 	// Create user interface
 	wxSizer* sizerFull= new wxBoxSizer(wxHORIZONTAL);
-	wxStaticBox* ss1Box = new wxStaticBox(this, -1, wxT("UI Input"));
-	wxStaticBoxSizer* ss1BoxS = new wxStaticBoxSizer(ss1Box, wxVERTICAL);
-	ss1BoxS->Add(new wxButton(this, id_button_ResetLiberty, wxT("Set Liberty Initial Transforms")), 0, wxALL, 1);
-	ss1BoxS->Add(new wxCheckBox(this, id_checkbox_ToggleLibertyRelMode, wxT("Use Liberty Relative Poses")), 0, wxALL, 1);
-	sizerFull->Add(ss1BoxS, 1, wxEXPAND | wxALL, 6);
+	viewer->camRadius = 3.0;
+	viewer->worldV += Vector3d(-0.3, 0.0, -0.8);
+	Matrix3d rotM; 
+	rotM << -0.8459, 0.0038246, 0.5332,0.000573691,-0.9999,0.008082, 0.533265, 0.00714295, 0.845918;
+	viewer->camRotT = rotM;
 	SetSizer(sizerFull);
+	frame->DoLoad("../../common/scenes/05-World-Teleop.urdf");
 
-    // ============================================================================
-    // Initialize grip stuff
-    viewer->camRadius = 3.0;
-    viewer->worldV += Vector3d(0.0, 0.0, -0.7);
-    viewer->UpdateCamera();
-    frame->DoLoad("../../common/scenes/04-World-Liberty.urdf");
-	if (mWorld == NULL)
-		frame->DoLoad("common/scenes/04-World-Liberty.urdf"); // for eclipse
+	// Create the timer to notify the function that draws the robot at multiple configurations
+	timer = new Timer();
+	timer->Start(1);
 
-    // Create the timer to notify the function that draws the robot at multiple configurations
-    timer = new Timer();
-    timer->Start(0.005 * 1e4);
+	// ============================================================================
+	// Initialize the ach stuff
 
-    // ============================================================================
-    // Initialize this daemon (program!)
-    somatic_d_opts_t dopt;
-    memset(&dopt, 0, sizeof(dopt)); // zero initialize
-    dopt.ident = "02-liberty-display";
-    somatic_d_init(&daemon_cx, &dopt);
+	// Initialize this daemon (program!)
+	somatic_d_opts_t dopt;
+	memset(&dopt, 0, sizeof(dopt)); // zero initialize
+	dopt.ident = "02-liberty-display";
+	somatic_d_init(&daemon_cx, &dopt);
 
-    // Initialize the liberty client
-    int channels[] = {0,1,2,3,4,5,6,7};
-    liberty.initLiberty(&daemon_cx, "liberty", 8, channels);
-
-    // Send a message; set the event code and the priority
-    somatic_d_event(&daemon_cx, SOMATIC__EVENT__PRIORITIES__NOTICE, 
-    SOMATIC__EVENT__CODES__PROC_RUNNING, NULL, NULL);
-
+	// Initialize the liberty client
+	somatic_d_channel_open(&daemon_cx, &liberty_chan, "liberty", NULL);
 }
 
 /* ********************************************************************************************* */
 SimTab::~SimTab() {
-
-    // Send the stopping event
-    somatic_d_event(&daemon_cx, SOMATIC__EVENT__PRIORITIES__NOTICE,
-		    SOMATIC__EVENT__CODES__PROC_STOPPING, NULL, NULL);
-
-    // Clean up the daemon resources
-    somatic_d_destroy(&daemon_cx);
+	somatic_d_destroy(&daemon_cx);
 }
 
 /* ********************************************************************************************* */
 void SimTab::GRIPEventSimulationBeforeTimestep() {}
-
-/* ********************************************************************************************* */
-/**
- * @function OnButton
- * @brief Handles button events
- */
-void SimTab::OnButton(wxCommandEvent &evt) {
-	int slnum = evt.GetId();
-	switch(slnum) {
-	// Set Start Arm Configuration
-	case id_button_ResetLiberty: {
-		liberty.setInitialPoses();
-		break;
-	}
-
-	case id_checkbox_ToggleLibertyRelMode: {
-		liberty_rel_mode = evt.IsChecked();
-		break;
-	}
-
-	default: {}
-	}
-}
-/* ********************************************************************************************* */
-void SimTab::OnSlider(wxCommandEvent &evt) {}
 
 /* ********************************************************************************************* */
 // Handler for events
@@ -149,7 +136,6 @@ void SimTab::OnSlider(wxCommandEvent &evt) {}
 BEGIN_EVENT_TABLE(SimTab, wxPanel)
 EVT_COMMAND (wxID_ANY, wxEVT_COMMAND_BUTTON_CLICKED, SimTab::OnButton)
 EVT_COMMAND (wxID_ANY, wxEVT_GRIP_SLIDER_CHANGE, SimTab::OnSlider)
-EVT_CHECKBOX(id_checkbox_ToggleLibertyRelMode, SimTab::OnButton)
 END_EVENT_TABLE()
 
 /* ********************************************************************************************* */
@@ -163,9 +149,9 @@ IMPLEMENT_DYNAMIC_CLASS(SimTab, GRIPTab)
 /// Application class
 extern wxNotebook* tabView;
 class mainApp : public GRIPApp {
-    virtual void AddTabs() {
+		virtual void AddTabs() {
 	tabView->AddPage(new SimTab(tabView), wxT("Liberty"));
-    }
+		}
 };
 
 IMPLEMENT_APP(mainApp)
